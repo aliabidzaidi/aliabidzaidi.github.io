afa137886c5b32683e6704b909cbd00deb282c82
Abid Zaidi <abidzaidi7@hotmail.com>
2020-04-09 23:33:15 +0500

    Adding Logo Name header + Flexbox design

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+    strings: ["Loading ...", ""],
+    typeSpeed: 100,
+    startDelay: 0,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "|",
+    contentType: 'html'
+});
